32d9863194b4c1906d85ecc2967dc36fbba78f0e
modarissa-ctrl <modar.issa@gmail.com>
2026-05-12 08:25:04 +0300

    Update admin.py
    To enable adding choices

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,8 +1,12 @@
-"""
-Admin interface configuration for the polls app.
-"""
-
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice, UserVote
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(UserVote)
